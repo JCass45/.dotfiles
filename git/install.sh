a11f8d594cb20b07b1bfa6ddb1ff3b21e37d2aa4
Jack Cassidy <jackc@qualtrics.com>
2019-10-27 14:19:38 +0000

    add: Git dotfiles

---
+++ git/install.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# better diffs
+if ! command -v diff-so-fancy >/dev/null 2>&1; then
+    echo "Run: 'brew install diff-so-fancy' for fancy diffs"
+else
+    git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+
+    git config --global color.ui true
+
+    git config --global color.diff-highlight.oldNormal    "red bold"
+    git config --global color.diff-highlight.oldHighlight "red bold 52"
+    git config --global color.diff-highlight.newNormal    "green bold"
+    git config --global color.diff-highlight.newHighlight "green bold 22"
+
+    git config --global color.diff.meta       "11"
+    git config --global color.diff.frag       "magenta bold"
+    git config --global color.diff.commit     "yellow bold"
+    git config --global color.diff.old        "red bold"
+    git config --global color.diff.new        "green bold"
+    git config --global color.diff.whitespace "red reverse"
+fi
