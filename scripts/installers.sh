8dadc0777b2e76baf40a8d4c54e6ac2af7a364cc
Jack Cassidy <jackc@qualtrics.com>
2019-10-29 17:28:37 +0000

    fix: Avoid running homebrew installer when running installers script

--- scripts/installers.sh
+++ scripts/installers.sh
@@ -4,6 +4,9 @@ set -e
 
 # find the installers and run them iteratively
 git ls-tree --name-only -r HEAD | grep install.sh | while read -r installer; do
+	if [ $installer = "homebrew/install.sh" ]; then
+		continue
+	fi
 	echo "› ${installer}..."
 	sh -c "$installer"
 done
