d9c66db812cbf9405137ef2596cdcabf6e618eca
Jack Cassidy <jackc@qualtrics.com>
2019-10-30 09:05:47 +0000

    update: iterm settings to load from custom folder

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 [ "$(uname -s)" != "Darwin" ] && exit 0
-sed "s;/Users/carlos;$HOME;g" \
-	"$HOME/dotfiles"/iterm/com.googlecode.iterm2.plist >"$HOME/dotfiles"/iterm/com.googlecode.iterm2.plist
 defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$HOME/dotfiles/iterm"
 defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
