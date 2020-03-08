3fb4208499a683ca6c80797b229d0bad58d79f7d
Jack Cassidy <jackc@qualtrics.com>
2020-03-08 21:44:14 +0000

    update: ls aliases to list items vertically

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if command -v lsd > /dev/null 2>&1; then
-    alias ls="lsd --group-dirs=last"
-    alias lsl="lsd -l --group-dirs=last"
-    alias lsa="lsd -a --group-dirs=last"
-    alias lsla="lsd -la --group-dirs=last"
+    alias ls="lsd --group-dirs=last -1"
+    alias lsl="lsd -l --group-dirs=last -1"
+    alias lsa="lsd -a --group-dirs=last -1"
+    alias lsla="lsd -la --group-dirs=last -1"
 else
     alias lsl="ls -l"
     alias lsa="ls -a"
