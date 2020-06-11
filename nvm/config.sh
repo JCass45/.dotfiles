89502b653d2e3b5ac88e82a7b03479ccd21ae016
Jack Cassidy <jackc@qualtrics.com>
2020-06-11 16:34:47 +0100

    update: NVM to auto use once it has been loaded

--- nvm/config.sh
+++ nvm/config.sh
@@ -2,4 +2,5 @@
 
 export NVM_DIR="$HOME/.nvm"
 export NVM_LAZY_LOAD=true
-# export NVM_AUTO_USE=true
+export NVM_AUTO_USE=true
+export NVM_COMPLETION=true
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if [[ ! -d "$ZSH/custom/plugins/zsh-syntax-highlighting" ]]; then
-    git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "$ZSH/custom/plugins/zsh-syntax-highlighting"
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]]; then
+    git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 fi
 
-if [[ ! -d "$ZSH/custom/plugins/zsh-completions" ]]; then
-    git clone git@github.com:zsh-users/zsh-completions.git "$ZSH/custom/plugins/zsh-completions"
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-completions" ]]; then
+    git clone git@github.com:zsh-users/zsh-completions.git "$ZSH_CUSTOM/plugins/zsh-completions"
+fi
+
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-nvm" ]]; then
+    git clone git@github.com:lukechilds/zsh-nvm.git "$ZSH_CUSTOM/plugins/zsh-nvm"
 fi
